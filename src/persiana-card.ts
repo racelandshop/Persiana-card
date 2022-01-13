634c0eba40b82036b1a1c58f74e92a6b4e01ecbc
jnog93 <joao.nogueira@raceland.pt>
2022-01-13 17:18:22 +0000

    alterações ao front-end de shutter

--- src/persiana-card.ts
+++ src/persiana-card.ts
@@ -51,7 +51,7 @@ export class BoilerplateCard extends LitElement {
 
   @queryAsync('mwc-ripple') private _ripple!: Promise<Ripple | null>;
 
-  setPickerPositionPercentage: any;
+  // setPickerPositionPercentage: any;
 
   public static getStubConfig(
     hass: HomeAssistant,
@@ -113,132 +113,281 @@ export class BoilerplateCard extends LitElement {
     let initialY;
     let xOffset = 0;
     let yOffset = 0;
+    this.config = this.config;
+    this.maxPosition = 137;
+    this.minPosition = 19;
+    this.isUpdating = false;
 
-    const mouseDown = (event) => {
-      if (event.cancelable) {
-        event.preventDefault();
-      }
-      this.isUpdating = true;
 
-      document.addEventListener('mousemove', mouseMove);
-      document.addEventListener('touchmove', mouseMove);
-      document.addEventListener('pointermove', mouseMove);
 
-      document.addEventListener('mouseup', mouseUp);
-      document.addEventListener('touchend', mouseUp);
-      document.addEventListener('pointerup', mouseUp);
-    }
+    if (!this.card) {
+      const card = document.createElement('hui-card');
 
-    let mouseMove = (event) => {
-      let newPosition = event.pageY - this.getItemTop(dragItem);
-      this.setPickerPosition(newPosition, picker, slide);
-    };
 
-    let mouseUp = function (this: any, event) {
-      this.isUpdating = false;
-
-      let newPosition = event.pageY - this.getItemTop(dragItem);
-
-      if (newPosition < this.minPosition)
-        newPosition = this.minPosition;
-
-      if (newPosition > this.maxPosition)
-        newPosition = this.maxPosition;
-
-      let percentagePosition = (newPosition - this.minPosition) * 100 / (this.maxPosition - this.minPosition);
-
-      let invertPercentage = false;
-      if (entityId && entityId.invert_percentage) {
-        invertPercentage = entityId.invert_percentage;
+      if (this.config.title) {
+        card.header = this.config.title;
       }
 
-      if (invertPercentage) {
-        this.updateBlindPosition(hass, entityId, percentagePosition);
-      } else {
-        this.updateBlindPosition(hass, entityId, 100 - percentagePosition);
-      }
+      this.card = card;
+      this.appendChild(card);
 
-      document.removeEventListener('mousemove', mouseMove);
-      document.removeEventListener('touchmove', mouseMove);
-      document.removeEventListener('pointermove', mouseMove);
 
-      document.removeEventListener('mouseup', mouseUp);
-      document.removeEventListener('touchend', mouseUp);
-      document.removeEventListener('pointerup', mouseUp);
-    };
+      let allBlinds = document.createElement('div');
+      allBlinds.className = 'svgicon-blind';
+      entities.forEach((entitiy) => {
 
-    picker.addEventListener('mousedown', mouseDown);
-    picker.addEventListener('touchstart', mouseDown);
-    picker.addEventListener('pointerdown', mouseDown);
-
-    let allBlinds = document.createElement('div');
-    allBlinds.className = 'svgicon-blind';
-
-    blind.querySelectorAll('svgicon-blind').forEach(function(button) {
-      button.onclick = function () {
-        const command = this.dataset.command;
-
-        let service = '';
-
-        switch (command) {
-          case 'up':
-            service = 'open_blinds';
-            break;
-
-          case 'down':
-            service = 'close_blinds';
-            break;
-
-          case 'stop':
-            service = 'stop_blinds';
-            break;
+        let entityId = entitiy;
+        if (entitiy && entitiy.entity) {
+          entityId = entitiy.entity;
         }
-        hass.callService('blinds', service, {
-          entityId: entityId
+
+        let buttonsPosition = 'left';
+        if (entitiy && entitiy.buttons_position) {
+          buttonsPosition = entitiy.buttons_position.toLowerCase();
+        }
+
+        let titlePosition = 'top';
+        if (entitiy && entitiy.title_position) {
+          titlePosition = entitiy.title_position.toLowerCase();
+        }
+
+        let invertPercentage = false;
+        if (entitiy && entitiy.invert_percentage) {
+          invertPercentage = entitiy.invert_percentage;
+        }
+
+        let blindColor = '#ffffff'
+        if (entitiy && entitiy.blind_color) {
+          blindColor = entitiy.blind_color;
+        }
+
+        let blind = document.createElement('div');
+
+        blind.className = 'svgicon-blind';
+        blind.dataset.blind = entityId;
+
+        blind.innerHTML = `
+          <div class="sc-blind-top" ` + (titlePosition == 'bottom' ? 'style="display:none;"' : '') +`>
+            <div class="sc-blind-label">
+
+            </div>
+            <div class="sc-blind-position">
+
+            </div>
+            </div>
+            <div class="sc-blind-middle" style="flex-direction: ` + (buttonsPosition == 'right' ? 'row-reserve' : 'row') + `;">
+              <div class="sc-blind-buttons">
+                <ha-icon-button class="sc-blind-button" data-command="up"><ha-icon icon="mdi:arrow-up"></ha-icon></ha-icon-button><br>
+                <ha-icon-button class="sc-blind-button" data-command="stop"><ha-icon icon="mdi:stop"></ha-icon></ha-icon-button><br>
+                <ha-icon-button class="sc-blind-button" data-command="down"><ha-icon icon="mdi:arrow-down"></ha-icon></ha-icon-button>
+              </div>
+              <div class0"sc-blind-selector">
+              <div class="sc-blind-selector-picture">
+              <div class="sc-blind-selector-slide"></div>
+              <div class="sc-blind-selector-picker"></div>
+            </div>
+            </div>
+            </div>
+            <div class="sc-blind-bottom" ` + (titlePosition != 'bottom' ? 'style="display:none;"' : '') + `>
+            <div class="sc-blind-label">
+
+            </div>
+            <div class="sc-blind-position">
+
+            </div>
+          </div>
+        `;
+
+        let dragItem = blind.querySelector('.sc-blind-selector-picture');
+        let slide = blind.querySelector('.sc-blind-selector-slide');
+        let picker = blind.querySelector('.sc-blind-selector-picker');
+
+        slide.style.background = blindColor;
+
+
+        const mouseDown = (event) => {
+          if (event.cancelable) {
+            event.preventDefault();
+          }
+          this.isUpdating = true;
+
+          document.addEventListener('mousemove', mouseMove);
+          document.addEventListener('touchmove', mouseMove);
+          document.addEventListener('pointermove', mouseMove);
+
+          document.addEventListener('mouseup', mouseUp);
+          document.addEventListener('touchend', mouseUp);
+          document.addEventListener('pointerup', mouseUp);
+        }
+
+        let mouseMove = (event) => {
+          let newPosition = event.pageY - this.getPictureTop(dragItem);
+          this.setPickerPosition(newPosition, picker, slide);
+        };
+
+        let mouseUp = function (this: any, event) {
+          this.isUpdating = false;
+
+          let newPosition = event.pageY - this.getPictureTop(dragItem);
+
+          if (newPosition < this.minPosition)
+            newPosition = this.minPosition;
+
+          if (newPosition > this.maxPosition)
+            newPosition = this.maxPosition;
+
+          let percentagePosition = (newPosition - this.minPosition) * 100 / (this.maxPosition - this.minPosition);
+
+          if (invertPercentage) {
+            this.updateBlindPosition(_homeassistant, entityId, percentagePosition);
+          } else {
+            this.updateBlindPosition(_homeassistant, entityId, 100 - percentagePosition);
+          }
+
+          document.removeEventListener('mousemove', mouseMove);
+          document.removeEventListener('touchmove', mouseMove);
+          document.removeEventListener('pointermove', mouseMove);
+
+          document.removeEventListener('mouseup', mouseUp);
+          document.removeEventListener('touchend', mouseUp);
+          document.removeEventListener('pointerup', mouseUp);
+        };
+
+        picker.addEventListener('mousedown', mouseDown);
+        picker.addEventListener('touchstart', mouseDown);
+        picker.addEventListener('pointerdown', mouseDown);
+
+        blind.querySelectorAll('.sc-blind-button').forEach(function (button) {
+          button.onclick = function () {
+            const command = this.dataset.command;
+
+            let service = '';
+
+            switch (command) {
+              case 'up':
+                service = 'open_blinds';
+                break;
+
+              case 'down':
+                service = 'close_blinds';
+                break;
+
+              case 'stop':
+                service = 'stop_blinds';
+                break;
+            }
+            _homeassistant.callService('blinds', service, {
+              entityId: entityId
+            });
+          };
         });
-      };
-    });
 
-    const style = document.createElement('style');
-    const state = hass.states[entityId];
-    const friendlyName = (entityId && entityId.name) ? entityId.name : state ? state.attributes.friendly_name : 'unknown';
-    const currentPosition = state ? state.attributes.current_position : 'unknown';
+        allBlinds.appendChild(dragItem);
 
-    allBlinds.appendChild(dragItem);
+        const style = document.createElement('style');
+        style.textContent = `
+          .sc-blinds {padding: 16px; }
+            .sc-blind { margin-top: 1rem; overflow: hidden; }
+            .sc-blind:first-child { margin-top: 0; }
+            .sc-blind-middle {display: flex; width: 210px; margin: auto; }
+              .sc-blind-buttons { flex: 1; text-align: center; margin-top: 0.4rem; }
+              .sc-blind-selector { flex: 1; }
+                .sc-blind-selector-picture { position: relative; margin: auto; background-size: blinds; min-height: 150px; max-height:100%; width: 153px; }
+                  .sc-blind-selector-picture { background-image: url(data:@file/png;base64,shutter_open) }
+                .sc-blind-selector-slide { background-color: #ffffff; position: absolute; top: 19px; left: 6px; width: 92.5%; height: 0; }
+                .sc-blind-selector-picker { position: absolute; top: 19px; left: 5px; width: 93.5%; cursor: pointer; height: 20px; background-repeat: no-repeat; }
+                  .sc-blind-selector-picker { background-image: url(data:@file/png;base64,shutter_close) }
+            .sc-blind-top { text-align: center; margin-bottom: 1rem; }
+            .sc-blind-bottom { text-align: center; margin-top: 1rem; }
+              .sc-blind-label { display: inline-block; font-size: 20px; text-align: center; vertical-align: middle; horizontal-align: middle; }
+              .sc-blind-position { display: inline-block; vertical-align: middle; horizontal-align: left; padding: 0 6 px; margin-left: 1rem; border-radius: 2px; background-color: var(--secondary-background-color); }
+        `;
 
-    this.card.appendChild(allBlinds);
-          this.appendChild(style);
+        this.card.appendChild(allBlinds);
+        this.appendChild(style);
 
-    updateBlindPosition(hass, entityId, position); {
-        let blindPosition = Math.round(position);
+        entities.forEach(function (entity) {
+          let entityId = entity;
+          if (entity && entity.entity) {
+            entityId = entity.entity;
+          }
 
-        hass.callService('blinds', 'set_cover_position', {
-          entity_id: entityId,
-          position: blindPosition
+          let invertPercentage = false;
+          if (entity && entity.invert_percentage) {
+            invertPercentage = entity.invert_percentage;
+          }
+
+          const blind = _this.card.querySelector('div[data-blind="' + entityId + '"]');
+          const slide = blind.querySelector('.sc-blind-selector-slide');
+          const picker = blind.querySelector('.sc-blind-selector-picker');
+
+          const state = _homeassistant.states[entityId];
+          const friendlyName = (entity && entity.name) ? entity.name : state ? state.attributes.friendly_name : 'unknown';
+          const currentPosition = state ? state.attributes.current_position : 'unknown';
+
+          blind.querySelectorAll('.sc-blind-label').forEach(function(blindLabel) {
+            blindLabel.innerHTML = friendlyName;
+          })
+
+          if (!_this.isUpdating) {
+            blind.querySelectorAll('.sc-blind-position').forEach(function (blindPosition) {
+              blindPosition.innerHTML = currentPosition + '%';
+            })
+
+            if (invertPercentage) {
+              _this.setPickerPositionPercentage(currentPosition, picker, slide);
+            } else {
+              _this.setPickerPositionPercentage(100 - currentPosition, picker, slide);
+            }
+          }
         });
+
+        getPictureTop(dragItem); {
+          let pictureBox = dragItem.getBoundingClientRect();
+          let body = document.body;
+          let docEl = document.documentElement;
+
+          let scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+
+          let clientTop = docEl.clientTop || body.clientTop || 0;
+
+          let pictureTop = pictureBox.top + scrollTop - clientTop;
+
+          return pictureTop;
+        }
+      });
     }
   }
 
-  //   function drag(e) {
-  //     if (active) {
-  //       e.preventDefault();
-  //       if (e.type === "touchmove") {
-  //         currentX = e.touches[0].clientX - initialX;
-  //         currentY = e.touches[0].clientY - initialY;
-  //       } else {
-  //         currentX = e.clientX - initialX;
-  //         currentY = e.clientY - initialY;
-  //       }
-  //       xOffset = currentX;
-  //       yOffset = currentY;
-  //       setTranslate(currentX, currentY, dragItem);
-  //     }
-  //   }
+  setPickerPositionPercentage(position, picker, slide): void {
+    let realPosition = (this.maxPosition - this.minPosition) * position / 100 + this.minPosition;
 
-  //   function setTranslate(xPos, yPos, el) {
-  //     el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-  //   }
-  // }
+    this.setPickerPosition(realPosition, picker, slide);
+  }
+
+  setPickerPosition(position, picker, slide) {
+    if (position < this.minPosition)
+      position = this.minPosition;
+
+    if (position > this.maxPosition)
+      position = this.maxPosition;
+
+    picker.style.top = position + 'px';
+    slide.style.height = position - this.minPosition + 'px';
+  }
+
+  updateBlindPosition(_homeassistant, entityId, position) {
+    let blindPosition = Math.round(position);
+
+    _homeassistant.callService('blinds', 'set_blinds_position', {
+      entity_id: entityId,
+      position: blindPosition
+    });
+    }
+
+    getCardSize() {
+      return this.config.entities.length + 1;
+    }
 
 public translate_state(stateObj): string {
     if (ifDefined(stateObj ? this.computeActiveState(stateObj) : undefined) === "on") {
@@ -386,33 +535,6 @@ public translate_state(stateObj): string {
     `;
   }
 
-
-
-
-  // private _cardUp(): void {
-  //   const lovelace = this.lovelace!;
-  //   const path = this.path!;
-  //   lovelace.saveConfig(
-  //     swapCard(lovelace.config, path, [path[0], path[1] - 1])
-  //   );
-  // }
-
-  // private _cardDown(): void {
-  //   const lovelace = this.lovelace!;
-  //   const path = this.path!;
-  //   lovelace.saveConfig(
-  //     swapCard(lovelace.config, path, [path[0], path[1] + 1])
-  //   );
-  // }
-
-  // private _cardStop(): void {
-  //   const lovelace = this.lovelace!;
-  //   const path = this.path!;
-  //   lovelace.saveConfig(
-  //     swapCard(lovelace.config, path, [path[0], path[0] + 0])
-  //   );
-  // }
-
 private computeActiveState = (stateObj: HassEntity): string => {
   const domain = stateObj.entity_id.split(".")[0];
   let state = stateObj.state;
@@ -486,7 +608,6 @@ private computeActiveState = (stateObj: HassEntity): string => {
       }
 
       ha-icon {
-        cursor: pointer;
         width: 70%;
         height: 80%;
         padding-bottom: 15px;
@@ -618,7 +739,6 @@ private computeActiveState = (stateObj: HassEntity): string => {
 
       /* alteração ao aspeto persiana */
       .state-off-blind-icon {
-        /* transform: translateY(0%); */
         animation-direction: reverse;
         transition: all 0.5s ease;
         fill: #a9b1bc;
@@ -633,7 +753,6 @@ private computeActiveState = (stateObj: HassEntity): string => {
 
       /* alteração ao aspeto persiana */
       .state-off-shutter-icon {
-        /* transform: translateY(0%); */
         animation-direction: reverse;
         transition: all 0.5s ease;
         fill: #a9b1bc;
@@ -643,8 +762,6 @@ private computeActiveState = (stateObj: HassEntity): string => {
         color: var(--state-icon-unavailable-color, #bdbdbd);
       }
 
-      .move {cursor: move;}
-
       .opacity {
         animation: opacity 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
       }
@@ -653,31 +770,15 @@ private computeActiveState = (stateObj: HassEntity): string => {
         animation-direction: reverse;
       }
 
-      /* @keyframes state {
+      @keyframes state {
         0% {
-          transform: translate(0px, 10px);
-        }
-        25% {
-          transform: translate(0px, 20px);
-        }
-        50% {
-          transform: translate(0px, 30px);
-        }
-        75% {
-          transform: translate(0px, 40px);
+          fill: #9da0a2;
         }
         100% {
-          transform: translate(0px, 50px);
+          fill: #b68349;
         }
-      } */
-
-      @keyframes stoper {
-        0% { transform: none; }
-        25% { transform: none; }
-        50% { transform: none; }
-        75% { transform: none; }
-        100% { transform: none; }
       }
+
 
       @keyframes opacity {
         0% {
@@ -690,10 +791,7 @@ private computeActiveState = (stateObj: HassEntity): string => {
     `;
   }
 }
-
-// function swapCard(_config: any, _path: any, _arg2: any[]): any {
-//   throw new Error("Function not implemented.");
-// }
+customElements.define("blind-card", BoilerplateCard);
 
 function hass(_hass: any, _entityId: any, _percentagePosition: number) {
   throw new Error("Function not implemented.");
@@ -715,3 +813,6 @@ function position(_hass: (_hass: any, _entityId: any, _percentagePosition: numbe
   throw new Error("Function not implemented.");
 }
 
+function getPictureTop(_dragItem: Element) {
+  throw new Error("Function not implemented.");
+}
